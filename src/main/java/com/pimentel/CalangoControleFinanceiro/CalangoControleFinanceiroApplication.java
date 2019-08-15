02c2bb3583ef5f62fc39d0e32e6b37c819e12c44
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2019-08-14 22:28:43 -0300

    Initial commit

---
+++ src/main/java/com/pimentel/CalangoControleFinanceiro/CalangoControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.pimentel.CalangoControleFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalangoControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalangoControleFinanceiroApplication.class, args);
+	}
+
+}
